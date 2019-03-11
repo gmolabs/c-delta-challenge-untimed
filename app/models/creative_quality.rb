01d067077fc3d98aaf44b8f518fb56f8dbd40aee
Geoff Morris <geoffm@gmail.com>
2019-03-10 19:24:21 -0700

    Implement normalized_score and test for CreativeQuality

--- app/models/creative_quality.rb
+++ app/models/creative_quality.rb
@@ -5,10 +5,12 @@ class CreativeQuality < ApplicationRecord
 
   validates :name, :description, :color, presence: true
 
-  def max_score(questions: nil)
+  def max_score(questions = nil)
     questions.nil? ? questions = Question.all : questions
     questions.sum { |q| q.max_for(self) }
   end
 
-
+  def normalized_score(my_raw = raw_total, my_max = max_total)
+    (my_raw.fdiv(my_max) * 100.0).to_i.clamp(-100, 100)
+  end
 end
